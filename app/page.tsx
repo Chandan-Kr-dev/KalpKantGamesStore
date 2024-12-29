8d910dc826e077632a22fcc1c893e273764ea257
Chandan-kr-dev <chandanramgar@gmail.com>
2024-12-29 18:15:30 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Navbar from "./Components/Navbar";
 import Navbar2 from "./Components/Navbar2";
+import Home from "./pages/Home/page";
 
-export default function Home() {
+export default function App() {
   return (
     <main>
 
       <Navbar/>
       <Navbar2/>
+      <Home/>
     </main>
   );
 }
