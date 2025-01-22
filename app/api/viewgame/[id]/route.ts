8f84ce6fb2e6b3bf12ea3d992f8acc10da186e66
Chandan-kr-dev <chandanramgar@gmail.com>
2025-01-23 00:15:21 +0530

    please

--- app/api/viewgame/[id]/route.ts
+++ app/api/viewgame/[id]/route.ts
@@ -15,6 +15,10 @@ export async function GET(req:Request,context:{params:{id:string}} ){
                 id:parseInt(id,10),
             }
         });
+
+        if (!game) {
+            return NextResponse.json({ error: "Game not found" }, { status: 404 });
+          }
         return NextResponse.json({game},{status:200})
     } catch (error) {
         return NextResponse.json({error},{status:500})
