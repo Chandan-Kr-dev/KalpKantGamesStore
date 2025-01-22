7ec335ed4eb4a3c54b79dd1e9dc01b7032a27c89
Chandan-kr-dev <chandanramgar@gmail.com>
2025-01-23 00:33:52 +0530

    ho ja bhai

--- app/api/viewgame/[id]/route.ts
+++ app/api/viewgame/[id]/route.ts
@@ -1,13 +1,13 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req:Request,context:{params:{id:string}} ){
+export async function GET(req:Request,context:{params:Promise<{id:string}>} ){
     try {
         // const body=await req.json();
         // const {id}=body;
         
         // const id=parseInt(params.id,10);
-        const {id}=context.params;
+        const {id}=await context.params;
         
 
         const game=await db.uploadedgames.findUnique({
