67b81ab365f6b1b7aaede573c485a6e382491561
Chandan-kr-dev <chandanramgar@gmail.com>
2025-02-17 16:16:21 +0530

    fixes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,16 +8,16 @@ export default async function sitemap():Promise<MetadataRoute.Sitemap>{
 
     return[
         {
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}`
+            url:`https://kalpkantgames.vercel.app/`
         },
         {
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}game/4`
+            url:`https://kalpkantgames.vercel.app/game/4`
         },
         {
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}game/10`
+            url:`https://kalpkantgames.vercel.app/game/10`
         },
         {
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}game/a85b2229-0ac8-48d5-a5ff-45737a9c56d8`
+            url:`https://kalpkantgames.vercel.app/game/a85b2229-0ac8-48d5-a5ff-45737a9c56d8`
         },
     ]
 }
