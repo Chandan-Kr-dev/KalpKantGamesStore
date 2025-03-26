c732413fbaf7c6accacdedaf36b2db2564dcc98c
Chandan-kr-dev <chandanramgar@gmail.com>
2025-03-27 00:57:13 +0530

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: require.resolve("react"),
-      "react/jsx-runtime": require.resolve("react/jsx-runtime"),
-    };
-    return config;
-  },
+  
   /* config options here */
   images: {
     remotePatterns: [
