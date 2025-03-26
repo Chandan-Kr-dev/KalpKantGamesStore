3355bab052022144dddca2d0298df14a4e94acb2
Chandan-kr-dev <chandanramgar@gmail.com>
2025-03-27 00:11:59 +0530

    google analytics added:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,15 +15,14 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default:"Kalpkant Games Store",
-    template:"%s - Kalpkant games"
-
+    default: "Kalpkant Games Store",
+    template: "%s - Kalpkant games",
   },
   description: "Your Gateway to infinite adventures",
-  other:{
+  other: {
     "google-adsense-account": "ca-pub-1674197696050997",
-    "google-site-verification" :"8eSvh0en8XRrVhaOShyVibev8sB4zX7D4hdqlFBx2qw" 
-  }
+    "google-site-verification": "8eSvh0en8XRrVhaOShyVibev8sB4zX7D4hdqlFBx2qw",
+  },
 };
 
 export default function RootLayout({
@@ -33,6 +32,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-P2FL06P3XR"
+        ></script>
+        <script >
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P2FL06P3XR');`}
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
